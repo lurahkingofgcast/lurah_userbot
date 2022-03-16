2f247ab3e5859aee3a9a4ea1aa8e7a6c9dc236e6
lurahkingofgcast <100414111+lurahkingofgcast@users.noreply.github.com>
2022-03-17 02:51:27 +0700

    Update and rename userbot/modules/gabut.py to bot pengguna/modul/gabut.py

--- bot pengguna/modul/gabut.py
+++ bot pengguna/modul/gabut.py
@@ -7,31 +7,31 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern="^.p(?: |$)(.*)")
+@register(outgoing=True, pattern="^.lrh1(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`𝙎𝙝𝙖𝙡𝙤𝙢...`")
+    await typew.edit("`𝐀𝐍𝐉𝐀𝐘 𝐋𝐔𝐑𝐀𝐇 𝐌𝐄𝐍𝐀𝐍𝐆🐣`")
 # Salam
 
 
-@register(outgoing=True, pattern="^.l(?: |$)(.*)")
+@register(outgoing=True, pattern="^.lrhh(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`𝙊𝙢 𝙎𝙬𝙖𝙨𝙩𝙞𝙖𝙨𝙩𝙪...`")
+    await typew.edit("`𝐃𝐈𝐌𝐀𝐍𝐀 𝐀𝐃𝐀 𝐁𝐀𝐍𝐒𝐎𝐒.....𝐃𝐈 𝐒𝐈𝐓𝐔 𝐀𝐃𝐀 𝐋𝐔𝐑𝐀𝐇🐣`")
 # Menjawab Salam
 
 
-@register(outgoing=True, pattern="^.istigfar(?: |$)(.*)")
+@register(outgoing=True, pattern="^.lrh(?: |$)(.*)")
 async def perkenalan(event):
     event.pattern_match.group(1)
-    await event.edit("`𝘼𝙨𝙩𝙖𝙜𝙖𝙖...`")
+    await event.edit("`𝐀𝐍𝐉𝐀𝐘 𝐀𝐃𝐀 𝐁𝐀𝐍𝐒𝐎𝐒,𝐋𝐔𝐑𝐀𝐇 𝐆𝐈𝐊𝐄𝐒 𝐃𝐔𝐋𝐔🐣.....`")
 # Istigfar
 
 
-@register(outgoing=True, pattern="^.perkenalan(?: |$)(.*)")
+@register(outgoing=True, pattern="^.lrh2(?: |$)(.*)")
 async def perkenalan(event):
     event.pattern_match.group(1)
-    await event.edit(f"`𝙃𝙖𝙞 𝙂𝙪𝙮𝙨 , 𝙋𝙚𝙧𝙠𝙚𝙣𝙖𝙡𝙠𝙖𝙣 𝙉𝙖𝙢𝙖 𝙂𝙬 {DEFAULTUSER}`")
+    await event.edit(f"`𝐀𝐍𝐉𝐀𝐘 𝐋𝐔𝐑𝐀𝐇 𝐂𝐀𝐊𝐄𝐏🐣`")
     sleep(2)
     await event.edit(f"`𝙂𝙬 𝙏𝙞𝙣𝙜𝙜𝙖𝙡 𝘿𝙞 {WEATHER_DEFCITY}`")
     sleep(2)
@@ -43,10 +43,10 @@ async def perkenalan(event):
 
 CMD_HELP.update({
     "gabut": "**Modules** - `Gabut`\
-    \n\n Cmd : `.l`\
-    \nUsage : Untuk Menjawab Salam\
-    \n\n Cmd : `.perkenalan`\
+    \n\n Cmd : `.lrh`\
+    \nUsage : Lurah Gikes\
+    \n\n Cmd : `.lrh2`\
     \nUsage : Memperkenalkan Diri\
-    \n\n Cmd : `.p`\
-    \nUsage : Untuk Memberi Salam."
+    \n\n Cmd : `.lrhh`\
+    \nUsage : bansos."
 })
